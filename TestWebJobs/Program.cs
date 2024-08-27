b2e7ffa5d4929c9f5bc4d5ff4c3312ccdaed4075
Darin Loftus (Insight Global LLC) <darin.loftus832@gmail.com>
2024-08-27 15:00:32 -0600

    Create Program.cs

---
+++ TestWebJobs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace TestWebJobs
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?linkid=2250384
+    internal class Program
+    {
+        // Please set AzureWebJobsStorage connection strings in appsettings.json for this WebJob to run.
+        public static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .ConfigureWebJobs(b =>
+                {
+                    b.AddAzureStorageCoreServices()
+                    .AddAzureStorageQueues();
+                })
+                .ConfigureLogging((context, b) =>
+                {
+                    b.SetMinimumLevel(LogLevel.Information);
+                    b.AddConsole();
+                });
+
+
+            var host = builder.Build();
+            using (host)
+            {
+                // The following code ensures that the WebJob will be running continuously
+                await host.RunAsync();
+            }
+        }
+    }
+}
+
+
